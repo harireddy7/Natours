839d44fe2fa6caec7a0fb2801e46b1634c5414f2
harireddy7 <harimsd8@gmail.com>
2021-09-19 18:36:35 +0530

    refactored .env files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 
 const app = require('./app')
 
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: './.env' })
 // console.log(process.env)
 
 // MONGOOSE CONNECTION
